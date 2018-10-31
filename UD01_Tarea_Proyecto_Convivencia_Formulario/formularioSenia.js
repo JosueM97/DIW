d7d251beae2d55f40f2da23dc209d5323a345177
Josue <josu.manez@gmail.com>
2018-10-31 19:05:27 +0100

    Resuelto lo del submit

--- UD01_Tarea_Proyecto_Convivencia_Formulario/formularioSenia.js
+++ UD01_Tarea_Proyecto_Convivencia_Formulario/formularioSenia.js
@@ -1,6 +1,7 @@
 function onSubmit( form ){
     
     var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
+    console.log("Saluda");
     console.log( data );
     return false; //don't submit
   }
@@ -9,9 +10,10 @@ function onSubmit( form ){
   function mostrarLleu(){
 
     document.getElementById("formulario1").style.visibility="visible";
-    
+    document.getElementById("formulario2").style.visibility="hidden";
   }
 
-  function ocultarLleu(){
+  function mostrarGreu(){
+    document.getElementById("formulario2").style.visibility="visible";
     document.getElementById("formulario1").style.visibility="hidden";
   }
