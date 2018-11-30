294ec36214900be1d77ec587dda7cfafb6df8b86
Josue <josu.manez@gmail.com>
2018-11-30 19:02:54 +0100

    Muchas cosas

--- UD01_Tarea_Proyecto_Convivencia_Formulario/servidorSenia/public/importarIncidencias.js
+++ UD01_Tarea_Proyecto_Convivencia_Formulario/servidorSenia/public/importarIncidencias.js
@@ -2,12 +2,53 @@ $.getJSON("http://localhost:3000/incidencias", function (data) {
   var items = [];
   $.each(data, function (key, val) {
     console.log(key + val.nombreAlumno);
-    items.push("<tr><td> " + val.nombreAlumno + "</td></tr>");
+
+    items.push("<div class=divAzul>");
+      
+      items.push("<div class=tituloAlumno>");
+      items.push("Alumno:");
+      items.push("</div>");
+
+      items.push("<br/>");
+
+      items.push("<div class=tituloProfesor>");
+      items.push("Profesor:");
+      items.push("</div>");
+
+      items.push("<br/>");
+      
+      items.push("<div class=tituloFecha>");
+      items.push("Fecha:");
+      items.push("</div>");
+
+      items.push("<br/>");
+    
+      items.push("<div class=divAlumno>");
+      items.push(val.nombreAlumno);
+      items.push("</div>");
+
+      items.push("<br/>");
+
+      items.push("<div class=divProfesor>");
+      items.push(val.nombreProfesor);
+      items.push("</div>");
+
+      items.push("<br/>");
+
+      items.push("<div class=divFecha>");
+      items.push(val.fecha);
+      items.push("</div>");
+
+      items.push("<br/>");
+
+      items.push("<img class=flecha src=flecha.jpg>");
+    items.push("</div>");
+
   });
 
 
-  $("<table/>", {
-    "class": "table table-striped table-dark",
+  $("<div/>", {
+    "class": "div",
     html: items.join("")
-  }).appendTo("#divListaLeves");
+  }).appendTo("#divListarTodo");
 });
